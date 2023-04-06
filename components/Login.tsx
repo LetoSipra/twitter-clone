a16f28ff7362a1e4956bedf068d752dd811e228d
LetoSipra(Yusuf) <yusufakcay22.0@gmail.com>
2023-04-06 03:08:22 +0300

    update

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,14 +1,19 @@
+import { Provider } from "next-auth/providers";
 import { signIn } from "next-auth/react";
 
-function Login({ providers }: any) {
+interface Props {
+  providers: Provider;
+}
+
+function Login({ providers }: Props) {
   return (
     <>
       <div className="flex flex-col items-center space-y-20 pt-48 text-white">
         <div>
-          {Object.values(providers).map((provider): any => (
+          {Object.values(providers).map((provider) => (
             <div key={provider.name}>
               <button onClick={() => signIn(provider.id, { callbackUrl: "/" })}>
-                3133333333333333
+                Click to login
               </button>
             </div>
           ))}
