b94cf65179bee7c68b9f72eb5293f97df6baec2b
LetoSipra(Yusuf) <yusufakcay22.0@gmail.com>
2023-04-12 02:16:00 +0300

    loginUpdate

--- components/Login.tsx
+++ components/Login.tsx
@@ -11,9 +11,14 @@ function Login({ providers }: Props) {
       <div className="flex flex-col items-center space-y-20 pt-48 text-white">
         <div>
           {Object.values(providers).map((provider) => (
-            <div key={provider.name}>
-              <button onClick={() => signIn(provider.id, { callbackUrl: "/" })}>
-                Click to login
+            <div
+              key={provider.name}
+              className="flex flex-col items-center space-y-5">
+              <p className="text-3xl tracking-wider">You need to login...</p>
+              <button
+                className="hover border border-white/30 px-5 py-3 transition duration-200 hover:border-white hover:bg-white/10"
+                onClick={() => signIn(provider.id, { callbackUrl: "/" })}>
+                Google
               </button>
             </div>
           ))}
